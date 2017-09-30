b778d307f8a16d72661f78dff2b6a18f2324a718
Shantanu Bala <shantanubala@gmail.com>
2017-09-30 15:00:51 -0500

    updated compilation to include cli

--- compile.sh
+++ compile.sh
@@ -2,3 +2,6 @@ python tools/build.py --jerry-libc=off --jerry-libm=off --jerry-cmdline=off --sn
 
 rm dist/libjerry-core.a
 cp build/lib/libjerry-core.a dist/
+
+python tools/build.py --jerry-libc=on --jerry-libm=on --jerry-cmdline=on --snapshot-exec=ON --snapshot-save=on --mem-heap=18 --clean
+
