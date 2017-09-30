2e0f49502668f14a1be3ca190a7bee7191d0b370
Shantanu Bala <shantanubala@gmail.com>
2017-09-30 15:01:59 -0500

    updated compilation script

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
+#! /usr/bin/env bash
+
 python tools/build.py --jerry-libc=off --jerry-libm=off --jerry-cmdline=off --snapshot-exec=ON --snapshot-save=off --mem-heap=18 --clean --toolchain=./cmake/toolchain_mcu_nrf52832.cmake
 
 rm dist/libjerry-core.a
